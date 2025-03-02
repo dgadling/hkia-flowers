26ce48579277a33f70a929ca25194f8caa4ebd10
Dave Gadling <dave@toasterwaffles.com>
2025-03-02 13:26:44 -0500

    Brand new, empty project. New name and everything

---
+++ FlowerFriend/FlowerFriend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowerFriend
+//
+//  Created by David Gadling on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowerFriend/FlowerFriend/FlowerFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerFriendApp.swift
+//  FlowerFriend
+//
+//  Created by David Gadling on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowerFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
